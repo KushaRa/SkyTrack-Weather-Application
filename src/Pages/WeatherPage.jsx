ad50ee005e5038cf4ce71308b453e62187a19275
kushana <rashmisenadheera98@gmail.com>
2024-07-14 22:52:28 +0530

    install axios and set the useState

--- src/Pages/WeatherPage.jsx
+++ src/Pages/WeatherPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import sun from '../Assets/sun.png';
 import humidity from '../Assets/humidity.png';
@@ -6,6 +6,18 @@ import windy from '../Assets/windy.png';
 import './WeatherPage.css';
 
 const Weather = () => {
+  const[data,setData]=useState(
+    {
+      celcius:25,
+      name:colombo,
+      humidity:84,
+      speed:2
+    }
+  )
+  useEffect(()=>
+  {
+    
+  })
   return (
     <div className="container">
       <div className="weather">
@@ -23,14 +35,19 @@ const Weather = () => {
           
           <div className="details">
               <div className="col">
-              <img src={humidity} alt='humidity' style={{ width: '100px', height: '100px', paddingTop:'30px'}}/>
+              <img src={humidity} alt='humidity' style={{ width: '75px', height: '75px', paddingTop:'30px'}}/>
+              <div className='humidity'>
               <p><b>Humidity: </b></p>
               <p><b>20%</b></p>
               </div>
+              </div>
+
               <div className="col">
-              <img src={windy} alt='windy' style={{ width: '100px', height: '100px', paddingTop:'30px'}}/>
-              <p><b>Windy: </b> </p>
+              <img src={windy} alt='windy' style={{ width: '75px', height: '75px', paddingTop:'30px'}}/>
+              <div className='wind'>
+              <p><b>Wind: </b> </p>
               <p><b>2Km/h</b></p>
+              </div>
               </div>
           </div>
 
