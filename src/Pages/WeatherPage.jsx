4e49bbd55791bf525c06567ae8a6d2c2eb2a7573
kushana <rashmisenadheera98@gmail.com>
2024-07-14 23:53:28 +0530

    set default data

--- src/Pages/WeatherPage.jsx
+++ src/Pages/WeatherPage.jsx
@@ -19,10 +19,12 @@ const Weather = () => {
   {
     const apiUrl='https://api.openweathermap.org/data/2.5/weather?q=colombo&appid=687b65438a15dac43046fa1060345ce2&&units=metric';
     axios.get(apiUrl)
-    .then(res => console.log(res))
+    .then(res => {
+      setData({...data, celcius: res.data.main.temp, name: res.data.name, humidity: res.data.main.humidity, speed: res.data.wind.speed})
+    })
     .catch(err=> console.log(err));
 
-  },[])
+  }, [])
   return (
     <div className="container">
       <div className="weather">
@@ -34,8 +36,8 @@ const Weather = () => {
 
           <div className="winfo">  
           <img src={sun} alt='sun' style={{ width: '100px', height: '100px', paddingTop:'30px'}} />
-          <h1>28°C</h1>
-          <h2>Colombo</h2>
+          <h1>{data.celcius}°C</h1>
+          <h2>{data.name}</h2>
           </div> 
           
           <div className="details">
@@ -43,7 +45,7 @@ const Weather = () => {
               <img src={humidity} alt='humidity' style={{ width: '75px', height: '75px', paddingTop:'30px'}}/>
               <div className='humidity'>
               <p><b>Humidity: </b></p>
-              <p><b>20%</b></p>
+              <p><b>{data.humidity}%</b></p>
               </div>
               </div>
 
@@ -51,7 +53,7 @@ const Weather = () => {
               <img src={windy} alt='windy' style={{ width: '75px', height: '75px', paddingTop:'30px'}}/>
               <div className='wind'>
               <p><b>Wind: </b> </p>
-              <p><b>2Km/h</b></p>
+              <p><b>{data.speed}Km/h</b></p>
               </div>
               </div>
           </div>
