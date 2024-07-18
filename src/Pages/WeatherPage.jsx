a29afdae84e3a77bdfd2e7cc0c4c73b8cf5d2939
kushana <rashmisenadheera98@gmail.com>
2024-07-19 00:22:35 +0530

    updated images and used switch state

--- src/Pages/WeatherPage.jsx
+++ src/Pages/WeatherPage.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import sun from '../Assets/sun.png';
+
 import humidity from '../Assets/humidity.png';
 import windy from '../Assets/windy.png';
+import drizzle from '../Assets/drizzle.png';
+import rain from '../Assets/rain.png';
+import mist from '../Assets/mist.png';
+import clear from '../Assets/clear.png';
 import './WeatherPage.css';
 import axios from 'axios';
+import clouds from '../Assets/clouds.png'
 
 const Weather = () => {
   const [data, setData] = useState({
@@ -12,7 +17,7 @@ const Weather = () => {
     name: 'Colombo',
     humidity: 84,
     speed: 2,
-    imagePath:"../Assets/windy.png"
+    image: clear // Default image
   });
 
   const [name, setName] = useState('');
@@ -22,11 +27,34 @@ const Weather = () => {
     const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=colombo&appid=687b65438a15dac43046fa1060345ce2&units=metric';
     axios.get(apiUrl)
       .then(res => {
+        let imagePath = windy; // Default image
+
+        switch (res.data.weather[0].main) {
+          case "Clouds":
+            imagePath = clouds;
+            break;
+          case "Drizzle":
+            imagePath = drizzle;
+            break;
+          case "Rain":
+            imagePath = rain;
+            break;
+          case "Mist":
+            imagePath = mist;
+            break;
+          case "Clear":
+            imagePath = clear;
+            break;
+          default:
+            imagePath = windy; // Default image
+        }
+
         setData({
           celcius: res.data.main.temp,
           name: res.data.name,
           humidity: res.data.main.humidity,
-          speed: res.data.wind.speed
+          speed: res.data.wind.speed,
+          image: imagePath
         });
       })
       .catch(err => console.log(err));
@@ -37,28 +65,34 @@ const Weather = () => {
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=687b65438a15dac43046fa1060345ce2&units=metric`;
       axios.get(apiUrl)
         .then(res => {
-          let imagePath ='';
-          if(res.data.weather[0].main === "Clouds"){
-            imagePath ="../Assets/windy.png"}
-          else if(res.data.weather[0].main === "Drizzel")
-            {imagePath ="../Assets/Drazzel.png"}
-          else if(res.data.weather[0].main === "Rain")
-            {imagePath ="../Assets/rain.png"}
-          else if(res.data.weather[0].main === "Mist")
-            {imagePath ="../Assets/Mist(2).png"}
-          else if(res.data.weather[0].main === "Clear")
-            {imagePath ="../Assets/sun.png"}
-          else{imagePath= "../Assets/windy.png"}
+          let imagePath = windy; // Default image
 
+          switch (res.data.weather[0].main) {
+            case "Clouds":
+              imagePath =windy;
+              break;
+            case "Drizzle":
+              imagePath = drizzle;
+              break;
+            case "Rain":
+              imagePath = rain;
+              break;
+            case "Mist":
+              imagePath = mist;
+              break;
+            case "Clear":
+              imagePath = clear;
+              break;
+            default:
+              imagePath = clear; // Default image
+          }
 
-          
-          console.log(res.data)
           setData({
             celcius: res.data.main.temp,
             name: res.data.name,
             humidity: res.data.main.humidity,
             speed: res.data.wind.speed,
-            image:imagePath
+            image: imagePath
           });
           setError('');
         })
@@ -87,7 +121,7 @@ const Weather = () => {
         {error && <p className="error">{error}</p>}
 
         <div className="winfo">
-          <img src={data.image} alt='sun' style={{ width: '100px', height: '100px', paddingTop: '30px' }} />
+          <img src={data.image} alt='weather' style={{ width: '100px', height: '100px', paddingTop: '30px' }} />
           <h1>{data.celcius}°C</h1>
           <h2>{data.name}</h2>
         </div>
@@ -97,15 +131,13 @@ const Weather = () => {
             <img src={humidity} alt='humidity' style={{ width: '75px', height: '75px', paddingTop: '30px' }} />
             <div className='humidity'>
               <p><b>Humidity: {data.humidity}% </b></p>
-              
             </div>
           </div>
 
           <div className="col">
-            <img src={windy} alt='windy' style={{ width: '75px', height: '75px', paddingTop: '30px' }} />
+            <img src={windy} alt='wind' style={{ width: '75px', height: '75px', paddingTop: '30px' }} />
             <div className='wind'>
               <p><b>Wind: {data.speed}Km/h </b></p>
-             
             </div>
           </div>
         </div>
